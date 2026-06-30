d1a27be41eafd050b73cb99af871233dee8a1438
Seu Nome <hannybaldow@gmail.com>
2026-06-29 21:00:10 -0300

    Adiciona placa e DS nas coletas

--- src/formatterColeta.js
+++ src/formatterColeta.js
@@ -4,8 +4,18 @@ export function gerarTextoColetas(coletas) {
 
   return `📦 FALHAS DE COLETA
 
-${coletas.map((rota) => `📍 #${rota.numero}
+${coletas.map((rota) => {
+
+  const ds = (
+    (rota.entregues / (rota.entregues + rota.falhas)) * 100
+  ).toFixed(1).replace(".", ",");
+
+  return `📍 #${rota.numero}
 🚐 ${rota.motorista}
-❌ ${rota.falhas} falhas`).join("\n\n")}`;
+🚗 ${rota.placa}
+📊 DS: ${ds}%
+❌ ${rota.falhas} falhas`;
+
+}).join("\n\n")}`;
 
 }
